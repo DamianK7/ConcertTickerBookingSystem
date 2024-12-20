9a32a991616e83348524567b2354c64c80bac5e5
Damian <damiankontakt7@gmail.com>
2024-12-20 22:29:10 +0100

    Dodanie interfejsu IConcert i klasy Concert

--- ConcertTickerBookingSystem/Program.cs
+++ ConcertTickerBookingSystem/Program.cs
@@ -1,2 +1,24 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+interface IConcert
+{
+    string Name { get; set; }
+    string Date { get; set; }
+    string Location { get; set; }
+    int AvailableSeats { get; set; }
+    decimal TicketPrice { get; set; }
+}
+
+class Concert : IConcert
+{
+    public string Name { get; set; }
+    public string Date { get; set; }
+    public string Location { get; set; }
+    public int AvailableSeats { get; set; }
+    public decimal TicketPrice { get; set; }
+
+    public virtual string GetDetails()
+    {
+        return $"{Name} - {Date} - {Location} - Miejsca: {AvailableSeats} - Cena: {TicketPrice} zł";
+    }
+}
